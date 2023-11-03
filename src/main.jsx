d5ca1b0b6d7dc621a348723d95f221d23c09f740
Dove-White <dugbazahredeemer@gmail.com>
2023-11-03 06:42:53 +0000

    add: nav to notfound page

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/vite-react-router/",
-    element: <App />,
-  },
-]);
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
